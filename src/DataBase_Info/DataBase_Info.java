4fe5f1ecd63c8f64961d64336639b7aaf1c8eb3b
周诚 <zhoucheng133@outlook.com>
2023-02-16 18:51:02 +0800

    增加了一些备注

--- src/DataBase_Info/DataBase_Info.java
+++ src/DataBase_Info/DataBase_Info.java
@@ -2,9 +2,11 @@ package DataBase_Info;
 
 public class DataBase_Info {
     // 数据库的用户和密码
+    // Mysql username and password
     static private String sqlName="root";
     static private String sqlPass="12345678";
     // 数据库的名称
+    // Database name
     static String dbName="graduassign";
     // Getters
     public static String getSqlName() {
